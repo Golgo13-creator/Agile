784181b218e754f7a022064668249fa8dc2448c2
laamaj92@gmail.com <laamaj92@gmail.com>
2021-04-16 17:33:06 +0200

    detail commit

---
+++ Agile.Models/MovieDetail.cs
@@ -0,0 +1,18 @@
+﻿using Agile.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agile.Models
+{
+    public class MovieDetail
+    {
+        public int MovieId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public GenreType Genre { get; set; }
+        public double AverageRating { get; set; }
+    }
+}
