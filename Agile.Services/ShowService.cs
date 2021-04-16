81ffb4e93dbfa154b72bb5d7f840e6ded36c580a
JacobStull <jrstull00@gmail.com>
2021-04-16 08:45:21 -0400

    Morning commit - table

---
+++ Agile.Services/ShowService.cs
@@ -0,0 +1,52 @@
+﻿using Agile.Data;
+using Agile.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agile.Services
+{
+    public class ShowService
+    {
+        private readonly Guid _userId;
+        public ShowService(Guid userId)
+        {
+            _userId = userId;
+        }
+        public bool CreateShow(PostShow model)
+        {
+            var entity =
+                new Show()
+                {
+                    Title = model.Title,
+                    Description = model.Description
+                };
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Shows.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public GetShow GetShowByTitle(string title)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .Shows
+                        .Single(e => e.Title == title);
+                return
+                    new GetShow
+                    {
+                        ShowId = entity.ShowId,
+                        Title = entity.Title,
+                        Description = entity.Description,
+                        Rating = entity.Rating,
+                    };
+            };
+        }
+    }
+}
---
+++ Agile.WebAPI/Controllers/ShowController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Agile.WebAPI.Controllers
+{
+    [System.Web.Authorize]
+    public class ShowController : ApiController
+    {
+        private ShowService CreateShowService()
+        {
+            var userId = Guid.Parse(userId.Identity.GetUserId());
+            var showService = new ShowService(userId);
+            return showService;
+        }
+
+        public IHttpActionResult Post(PostShow show)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var service = CreateShowService();
+            if (!service.CreateShow(show))
+                return InternalServerError();
+            return Ok();
+        }
+        //Get all Shows
+        public IHttpActionResult GetAllShows()
+        {
+            ShowServie showService = CreateShowService();
+            var shows = showService.GetShows();
+            return Ok(shows);
+        }
+        //Get Show by Title
+        public IHttpActionResult GetShowBytTitle(string title)
+        {
+            CreateShowService showService = CreateShowService();
+            var show = showService.GetMovieByTitle(title);
+            return Ok(title);
+
+        }
+
+    }
+}
